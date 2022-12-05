766b766447cb5d3f795dd186d62ad13adeec6da8
smallvalthoss <48263101+smallvalthoss@users.noreply.github.com>
2022-12-04 18:00:14 -0700

    Scaling arrow for impulse speed

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -13,26 +13,39 @@ public class ArrowController : MonoBehaviour
     [SerializeField] private Transform transformToRotateAround;
 
     private Coroutine rotateCo;
-    private bool isCoRunning;
+    private bool isSpeedCoRunning;
+
+    private Coroutine velocityCo;
+    private bool isVelocityCoRunning;
+
     private bool canRotate = true;
 
     private RotationEvent StartRotationEvent = new RotationEvent();
     private float angleMultiplier = -1f;
 
+    [SerializeField] private float maxScale = 1.0f;
+    [SerializeField] private float minScale = 0.3f;
+    [SerializeField] private float timeToScaleJawn = 2.0f;
+
+    private float chosenScale = 0.0f;
+    private Vector3 initialScale;
+
     private Quaternion initialRotation;
     [SerializeField]private MeshRenderer meshRenderer;
 
-    public Quaternion centerRotation;
+    [HideInInspector] public Quaternion centerRotation;
 
     private float offsetInFront = 1f;
     public float getOffset() { return offsetInFront; }
-    
+
 
     // Start is called before the first frame update
     void Awake()
     {
         centerRotation = transform.rotation;
         meshRenderer.enabled = false;
+        initialScale = new Vector3(1.0f, 1.0f, minScale);
+        transform.localScale = initialScale;
 
         // TODO: subscribe to event to start rotating
         StartRotationEvent.AddListener(StartRotation);
@@ -52,12 +65,8 @@ public class ArrowController : MonoBehaviour
 
     public void stopRotation()
     {
-        meshRenderer.enabled = false;
         canRotate = false;
-        if (isCoRunning)
-        {
-            StopCoroutine(rotateCo);
-        }
+        StopAllCoroutines();
     }
 
     public void StartRotation()
@@ -88,7 +97,7 @@ public class ArrowController : MonoBehaviour
 
     private IEnumerator RotateUntil(Quaternion targetRotation, float degreesPerSecond, RotationEvent toInvoke)
     {
-        isCoRunning = true;
+        isSpeedCoRunning = true;
 
         do
         {
@@ -99,7 +108,55 @@ public class ArrowController : MonoBehaviour
         } while (Quaternion.Angle(targetRotation, transform.rotation) > 0.5f);
         
 
-        isCoRunning = false;
+        isSpeedCoRunning = false;
         toInvoke.Invoke(timeToRotateCompletely);
+    }
+
+    public void StartChooseVelocity()
+    {
+        velocityCo = StartCoroutine(ChooseSpeed());
+    }
+
+    public void StopChooseVelocity()
+    {
+        
+        meshRenderer.enabled = false;
+        initialScale = new Vector3(1.0f, 1.0f, minScale);
+        transform.localScale = initialScale;
+        StopAllCoroutines();
+    }
+
+    public float getForcePercentage()
+    {
+        Debug.Log("local scale: " + transform.localScale);
+        Debug.Log("min scale: " + minScale);
+        Debug.Log("max scale: " + maxScale);
+
+        return (chosenScale - minScale) / (maxScale - minScale);
+    }
+
+    private IEnumerator ChooseSpeed()
+    {
+
+        Vector3 newScale = transform.localScale;
+
+        while (true)
+        {
+            for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / timeToScaleJawn)
+            {
+                chosenScale = Mathf.Lerp(minScale, maxScale, t);
+                newScale.z = chosenScale;
+                transform.localScale = newScale;
+                yield return null;
+            }
+
+            for (float t = 1.0f; t > 0.0f; t -= Time.deltaTime / timeToScaleJawn)
+            {
+                chosenScale = Mathf.Lerp(minScale, maxScale, t);
+                newScale.z = chosenScale;
+                transform.localScale = newScale;
+                yield return null;
+            }
+        }
     }
 }
